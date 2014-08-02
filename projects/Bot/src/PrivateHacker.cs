4084163c5f01268b435941f78c8f65add0250d28
Helios <Icarus@Deadalus.invalid>
2014-08-02 13:39:32 +0200

    simpler privatehacker

--- projects/Bot/src/PrivateHacker.cs
+++ projects/Bot/src/PrivateHacker.cs
@@ -9,77 +9,72 @@ namespace HearthstoneBot
 {
     public class PrivateHacker
     {
+        public static object get_private_field(object o, String name)
+        {
+            FieldInfo myFieldInfo = o.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            return myFieldInfo.GetValue(o);
+        }
+
+        public static void set_private_field(object o, String name, object val)
+        {
+            FieldInfo myFieldInfo = o.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            myFieldInfo.SetValue(o, val);
+        }
+
+        public static object call_private_method(object o, string name, object[] args)
+        {
+            MethodInfo dynMethod = o.GetType().GetMethod(name, BindingFlags.NonPublic | BindingFlags.Instance);
+            return dynMethod.Invoke(o, args);
+        }
+
         public static void ForceManaUpdate(Entity entity)
         {
-            InputManager input_mgr = InputManager.Get();
-            MethodInfo dynMethod = input_mgr.GetType().GetMethod("ForceManaUpdate", BindingFlags.NonPublic | BindingFlags.Instance);
-            dynMethod.Invoke(input_mgr, new object[] { entity });
+            call_private_method(InputManager.Get(), "ForceManaUpdate", new object[] { entity });
         }
 
         public static void HandleClickOnCardInBattlefield(Card c)
         {
-            Entity e = c.GetEntity();
-
-            InputManager input_mgr = InputManager.Get();
-            MethodInfo dynMethod = input_mgr.GetType().GetMethod("HandleClickOnCardInBattlefield", BindingFlags.NonPublic | BindingFlags.Instance);
-            dynMethod.Invoke(input_mgr, new object[] { e });
+            call_private_method(InputManager.Get(), "HandleClickOnCardInBattlefield", new object[] { c.GetEntity() });
         }
 
         public static void GrabCard(Card c)
         {
-            GameObject ob = c.gameObject;
-            InputManager input_mgr = InputManager.Get();
-            MethodInfo dynMethod = input_mgr.GetType().GetMethod("GrabCard", BindingFlags.NonPublic | BindingFlags.Instance);
-            dynMethod.Invoke(input_mgr, new object[] { ob });
+            call_private_method(InputManager.Get(), "GrabCard", new object[] { c.gameObject });
         }
 
         public static bool PlayPowerUpSpell(Card c)
         {
-            InputManager input_mgr = InputManager.Get();
-            MethodInfo dynMethod = input_mgr.GetType().GetMethod("PlayPowerUpSpell", BindingFlags.NonPublic | BindingFlags.Instance);
-            return (bool) dynMethod.Invoke(input_mgr, new object[] { c });
+            return (bool) call_private_method(InputManager.Get(), "PlayPowerUpSpell", new object[] { c });
         }
 
         public static bool PlayPlaySpell(Card c)
         {
-            InputManager input_mgr = InputManager.Get();
-            MethodInfo dynMethod = input_mgr.GetType().GetMethod("PlayPlaySpell", BindingFlags.NonPublic | BindingFlags.Instance);
-            return (bool) dynMethod.Invoke(input_mgr, new object[] { c });
+            return (bool) call_private_method(InputManager.Get(), "PlayPlaySpell", new object[] { c });
         }
 
         public static ZonePlay get_m_myPlayZone()
         {
-            InputManager input_mgr = InputManager.Get();
-            FieldInfo myFieldInfo = input_mgr.GetType().GetField("m_myPlayZone", BindingFlags.NonPublic | BindingFlags.Instance); 
-            return (ZonePlay) myFieldInfo.GetValue(input_mgr); 
+            return (ZonePlay) get_private_field(InputManager.Get(), "m_myPlayZone");
         }
 
         public static ZoneHand get_m_myHandZone()
         {
-            InputManager input_mgr = InputManager.Get();
-            FieldInfo myFieldInfo = input_mgr.GetType().GetField("m_myHandZone", BindingFlags.NonPublic | BindingFlags.Instance); 
-            return (ZoneHand) myFieldInfo.GetValue(input_mgr); 
+            return (ZoneHand) get_private_field(InputManager.Get(), "m_myHandZone");
         }
 
         public static ZoneWeapon get_m_myWeaponZone()
         {
-            InputManager input_mgr = InputManager.Get();
-            FieldInfo myFieldInfo = input_mgr.GetType().GetField("m_myWeaponZone", BindingFlags.NonPublic | BindingFlags.Instance); 
-            return (ZoneWeapon) myFieldInfo.GetValue(input_mgr); 
+            return (ZoneWeapon) get_private_field(InputManager.Get(), "m_myWeaponZone");
         }
 
         public static void set_m_battlecrySourceCard(Card val)
         {
-            InputManager input_mgr = InputManager.Get();
-            FieldInfo myFieldInfo = input_mgr.GetType().GetField("m_battlecrySourceCard", BindingFlags.NonPublic | BindingFlags.Instance); 
-            myFieldInfo.SetValue(input_mgr, val); 
+            set_private_field(InputManager.Get(), "m_battlecrySourceCard", val);
         }
 
         public static void set_m_lastZoneChangeList(ZoneChangeList val)
         {
-            InputManager input_mgr = InputManager.Get();
-            FieldInfo myFieldInfo = input_mgr.GetType().GetField("m_lastZoneChangeList", BindingFlags.NonPublic | BindingFlags.Instance); 
-            myFieldInfo.SetValue(input_mgr, val); 
+            set_private_field(InputManager.Get(), "m_lastZoneChangeList", val);
         }
     }
 }
